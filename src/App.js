c1e7b4c76430e3fab0c629e79971dc462ede6b77
Pupkin <bebeto123@live.ru>
2020-10-13 11:20:13 +0300

    test 1 commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.sass'
+import { Header } from './components/Header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Hello, Puppa!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.sass'
+
+const Header = (props) => {
+  return (
+    <div className="Header">
+      <p>Header</p>
+    </div>
+  )
+}
+
+export { Header }
